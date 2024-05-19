aa0fe410d1b6050d51e45349c8ee323a99304a3d
DoubleWoot <202110423@feualabang.edu.ph>
2024-05-19 20:06:15 +0800

    File Handling Added

--- src/components/RealTalk/Realtalk.tsx
+++ src/components/RealTalk/Realtalk.tsx
@@ -1,13 +1,36 @@
+import { useState } from 'react'
 import './Realtalk.css'
 
 export default function RealTalk(){
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = event.target.files && event.target.files[0];
+        if (selectedFile && selectedFile.type == 'audio/wav'){
+            setFile(selectedFile);
+        } else {
+            alert("Please upload a .wav file");
+        }
+    };
+
+    const handleFileUpload = () => {
+        if (file) {
+            // Figuring out how to integrate the model first
+            console.log('File uploaded', file.name);
+            // This for confirmation for file upload
+        }
+    }
+
     return (
         <>
         <div>
-            <h1 className="test">RealTalk</h1>
+            <h1 className="header1">RealTalk</h1>
             <div className="container">
                 <div className="sqaure file_upload">
                     <h2>Upload Your Audio</h2>
+                    <input type="file" accept=".wav" onChange={handleFileChange} />
+                    {file && <p> Select file: {file.name} </p>}
+                    <button onClick={handleFileUpload}>Upload</button>
                 </div>
                 <div className="sqaure file_report">
                     <h2>Progress Report</h2>
